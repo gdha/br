81d7dab596e4fe5a2a689a7ebb9f72a38f910b8a
D'Haese, Gratien [GTSBE - Non JJ] <GDHAESE1@ITS.JNJ.COM>
2025-01-02 16:19:36 +0100

    Implement advise of SonarQube

--- br.c
+++ br.c
@@ -16,16 +16,17 @@
 #include <stdio.h>
 #include <grp.h>
 #include <string.h>
+#include <errno.h>
 #include "br.h"
 
 
-int countStrings(char allowed_groups[][MAX_STR_LEN], int maxNumStrings) {
+int countStrings(char allowedGroups[][MAX_STR_LEN], int maxNumStrings) {
     int count = 0;
 
     // Iterate through the array until you encounter a null terminator
     for (int i = 0; i < maxNumStrings; i++) {
         // If the first character of the string is null, it means we've reached the end of the array
-        if (allowed_groups[i][0] == '\0') {
+        if (allowedGroups[i][0] == '\0') {
             break;
         }
 	count++;
@@ -44,8 +45,13 @@ int main(void) {
 
     // Define some variable to contain the group of the RUSER
     gid_t group_list[MAX_GROUP_STRINGS];
-    int num_groups, i;
-    struct group *grp;
+    int num_groups;             // Nummer of groups variable
+    char user[256];             // Buffer to hold the username
+    int result;                 // Contains exit code user lookup
+    // gid_t gid = getgid();       // Get the group ID of the current process
+    struct group grp;           // Group structure to store group details
+    struct group *groupResult;  // Pointer to hold the result of the group lookup
+    char buffer[1024];          // Buffer for additional group information
 
     // Get the number of groups the user is a member of
     num_groups = getgroups(MAX_GROUP_STRINGS, group_list);
@@ -56,38 +62,49 @@ int main(void) {
     }
 
     // Get the user's login name
-    char *user = getlogin();
-    if (user == NULL) {
-        perror("getlogin");
-        return 1;
+    result = getlogin_r(user, sizeof(user));
+    if (result == 0) {
+        // Success: user contains the logged-in user's name
+        // printf("logged in as %s\n", user);
+        ;
+    } else {
+        // Handle errors
+        if (result == ERANGE) {
+            fprintf(stderr, "Buffer size is too small for username\n");
+        } else {
+            perror("getlogin_r");
+        }
     }
 
-    // Count the aamount of listed groups in the allowed_groups array (see br.h header file)
+    // Count the amount of listed groups in the allowed_groups array (see br.h header file)
     int numAllowedGroups = countStrings(allowed_groups, MAX_GROUP_STRINGS);
-    // fprintf(stderr, "The numAllowedGroups is: %d\n", numAllowedGroups);
 
     // Iterate over each group the user is member of to check if the user belongs to it
-    for (i = 0; i < num_groups; i++) {
+    for (int i = 0; i < num_groups; i++) {
         // Get group entry by its ID
-        grp = getgrgid(group_list[i]);
-        if (grp == NULL) {
-            perror("getgrgid");
-            continue;
+        int ret = getgrgid_r(group_list[i], &grp, buffer, sizeof(buffer), &groupResult);
+        // grp = getgrgid_r(group_list[i]);
+        if (ret == 0) {
+            if (groupResult == NULL) {
+                // No group found for the given GID
+                printf("no group found for GID: %d\n", group_list[i]);
+            }
+        } else {
+            // Handle error
+            fprintf(stderr, "Error: %s\n", strerror(ret));
         }
 
         // Check if the user is a member (member is an username) of the group
-        char **members = grp->gr_mem;
+        char **members = grp.gr_mem;
 
-        // fprintf(stderr, "user = %s\n", user);
         while (*members != NULL) {
-            // fprintf(stderr, "group = %s : user = %s\n", grp->gr_name, *members);
 
             if (strcmp(*members, user) == 0) {
 		// igrp is an integer counter used for the amount of strings in array allowed_groups
 		for (int igrp = 0; igrp < numAllowedGroups; igrp++) {
-                    if (strcmp(grp->gr_name, allowed_groups[igrp]) == 0)  {
+                    if (strcmp(grp.gr_name, allowed_groups[igrp]) == 0)  {
                         // User is part of the 'wheel' group and is allowed to proceed
-                        printf("%s is a member of group %s (gid: %d) and allowed access.\n\n", user, grp->gr_name, grp->gr_gid);
+                        printf("%s is a member of group %s (gid: %d) and allowed access.\n\n", user, grp.gr_name, grp.gr_gid);
 
                         // Clear the environment
                         clearenv();
@@ -113,11 +130,10 @@ int main(void) {
                 } // end of for (int igrp = 0; igrp < numAllowedGroups; igrp++)
 	    } // end of if (strcmp(*members, user) == 0)
             members++;
-            // fprintf(stderr, "new member = %s\n", *members);
         }  // of while
     } // of for (i = 0; i < num_groups; i++)
 
-    // When you at this point the user was not part of the 'wheel' or 'gtsccon'  group so we say:
+    // When you at this point the user was not part of the 'wheel' group so we say:
     perror("Not authorized!");
     return 0;
 }
